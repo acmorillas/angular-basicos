415b2ab4bf93d5e6e4d9e05e892f889037e218d5
Andres Chamorro <achamoro@gmail.com>
2022-05-23 17:06:36 +0200

    Eliminacion de dependencias

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
-import { DbzService } from '../services/dbz.services';
 
 @Component({
   selector: 'app-main-page',
